731904fcb9acf0c788d00e6bceb7d5341512ff51
pairx-explains <202355046+pairx-explains@users.noreply.github.com>
2025-06-26 00:14:03 -0400

    batching implementation

--- core.py
+++ core.py
@@ -44,7 +44,7 @@ def get_intermediate_feature_maps_and_embedding(img, model, layer_keys):
 
 def get_feature_matches(feature_map_0, feature_map_1, img_0, img_1):
     def flatten_to_descriptors(feature_map):
-        descriptors = feature_map.squeeze().flatten(start_dim=1).transpose(0,1)
+        descriptors = feature_map.flatten(start_dim=1).transpose(0,1)
         return descriptors.detach().cpu().numpy()
 
     def get_keypoints(feature_map, img):
@@ -102,7 +102,7 @@ def get_intermediate_relevances(img, gradient, model, layer_keys):
         intermediate_relevances = {}
         def save_grad(name):
             def hook(module, grad_in, grad_out):
-                intermediate_relevances[name] = grad_out[0].squeeze().sum(dim=0).abs().detach().cpu().numpy()
+                intermediate_relevances[name] = grad_out[0].abs().sum(dim=1).detach().cpu().numpy()
 
             return hook
 
@@ -120,7 +120,7 @@ def get_intermediate_relevances(img, gradient, model, layer_keys):
         
     return intermediate_relevances
 
-def get_pixel_relevances(device, img, coords, model, layer_key):
+def get_pixel_relevances(device, img, coord_lists, model, layer_key):
     composite = EpsilonPlus(canonizers=[choose_canonizer(model)])
     
     img.requires_grad = True
@@ -132,13 +132,14 @@ def get_pixel_relevances(device, img, coords, model, layer_key):
         intermediate_fms, _ = get_intermediate_feature_maps_and_embedding(img, modified_model, [layer_key])
         intermediate_fm = intermediate_fms[layer_key]
 
-        for coord in coords:
-            gradient = torch.zeros(intermediate_fm.shape)
-            gradient[:, :, coord[1], coord[0]] = intermediate_fm[:, :, coord[1], coord[0]]
+        for coords in zip(*coord_lists):
+            gradient = torch.zeros(intermediate_fm.shape).to(device)
+            for i, coord in enumerate(coords):
+                gradient[i, :, coord[1], coord[0]] = intermediate_fm[i, :, coord[1], coord[0]]
 
             intermediate_fm.backward(gradient=gradient.to(device), retain_graph=True)
 
-            grads.append(img.grad.squeeze().sum(dim=0).abs().detach().cpu().numpy())
+            grads.append(img.grad.sum(dim=1).abs().detach().cpu().numpy())
             img.grad=None
 
     return grads
@@ -215,49 +216,57 @@ def draw_matches_and_color_maps(img_np_0, img_np_1, matches,
 
     return cv2.vconcat((matches_img, color_map_img))
 
-def pairx(device, img_0, img_1, model, layer_keys, k_lines, k_colors):
-    feature_maps_0, emb_0 = get_intermediate_feature_maps_and_embedding(img_0, model, layer_keys)
-    feature_maps_1, emb_1 = get_intermediate_feature_maps_and_embedding(img_1, model, layer_keys)
+def pairx(device, imgs_0, imgs_1, model, layer_keys, k_lines, k_colors):
+    feature_maps_0, emb_0 = get_intermediate_feature_maps_and_embedding(imgs_0, model, layer_keys)
+    feature_maps_1, emb_1 = get_intermediate_feature_maps_and_embedding(imgs_1, model, layer_keys)
     
     # backpropagate cosine similarity back to the intermediate layers
     emb_0.retain_grad()
     emb_1.retain_grad()
     
     cosine_sim = F.cosine_similarity(emb_0, emb_1, dim=1)
-    cosine_sim.backward()
+    cosine_sim.backward(gradient=cosine_sim)
 
-    intermediate_relevances_0 = get_intermediate_relevances(img_0, emb_0.grad, model, layer_keys)
-    intermediate_relevances_1 = get_intermediate_relevances(img_1, emb_1.grad, model, layer_keys)
+    intermediate_relevances_0 = get_intermediate_relevances(imgs_0, emb_0.grad, model, layer_keys)
+    intermediate_relevances_1 = get_intermediate_relevances(imgs_1, emb_1.grad, model, layer_keys)
 
     results = {}
     for layer_key in layer_keys:
         feature_map_0 = feature_maps_0[layer_key]
         feature_map_1 = feature_maps_1[layer_key]
+
         intermediate_relevance_0 = intermediate_relevances_0[layer_key]
         intermediate_relevance_1 = intermediate_relevances_1[layer_key]
-        
-        # get a set of feature matches
-        matches = get_feature_matches(feature_map_0, feature_map_1, img_0, img_1)
 
-        # go through matches and record each match's calculated relevance
-        for match in matches:
-            i0, j0 = match['coord0']
-            i1, j1 = match['coord1']
-            match['relevance'] = intermediate_relevance_0[j0][i0] * intermediate_relevance_1[j1][i1]
+        coord_lists_0 = []
+        coord_lists_1 = []
+        all_matches = []
+        for fm_0, fm_1, ir_0, ir_1 in zip(feature_map_0, feature_map_1, intermediate_relevance_0, intermediate_relevance_1):
+            matches = get_feature_matches(fm_0, fm_1, imgs_0, imgs_1)
 
-        matches.sort(key = lambda x: -x['relevance'])
+            # go through matches and record each match's calculated relevance
+            for match in matches:
+                i0, j0 = match['coord0']
+                i1, j1 = match['coord1']
+                match['relevance'] = ir_0[j0][i0] * ir_1[j1][i1]
+
+            matches.sort(key = lambda x: -x['relevance'])
+            coord_lists_0.append([match['coord0'] for match in matches[:k_colors]])
+            coord_lists_1.append([match['coord1'] for match in matches[:k_colors]])
+
+            all_matches.append(matches[:k_lines])
 
         # for each selected feature match, backpropagate to the original image
-        pixel_relevances_0 = get_pixel_relevances(device, img_0, [match['coord0'] for match in matches[:k_colors]], model, layer_key)
-        pixel_relevances_1 = get_pixel_relevances(device, img_1, [match['coord1'] for match in matches[:k_colors]], model, layer_key)
+        pixel_relevances_0 = get_pixel_relevances(device, imgs_0, coord_lists_0, model, layer_key)
+        pixel_relevances_1 = get_pixel_relevances(device, imgs_1, coord_lists_1, model, layer_key)
 
         results[layer_key] = {'intermediate_relevances': (intermediate_relevance_0, intermediate_relevance_1),
-                              'matches': matches[:k_lines],
+                              'matches': all_matches,
                               'pixel_relevances': (pixel_relevances_0, pixel_relevances_1)}
         
     return results
 
-def explain(img_0, img_1, img_np_0, img_np_1, model, layer_keys, k_lines=20, k_colors=10, background_opacity=0.2):
+def explain(imgs_0, imgs_1, imgs_np_0, imgs_np_1, model, layer_keys, k_lines=20, k_colors=10, background_opacity=0.2):
     """
     Generates a PAIR-X explanation for the provided images and model.
 
@@ -278,21 +287,28 @@ def explain(img_0, img_1, img_np_0, img_np_1, model, layer_keys, k_lines=20, k_c
     device = model.device
 
     # get pairx results
-    pairx_results = pairx(device, img_0, img_1, model, layer_keys, k_lines, k_colors)
+    pairx_results = pairx(device, imgs_0, imgs_1, model, layer_keys, k_lines, k_colors)
 
     # visualize the results into images
-    output_images = []
-    for layer_key in layer_keys:
-        matches = pairx_results[layer_key]["matches"]
-        intermediate_relevance_0, intermediate_relevance_1 = pairx_results[layer_key]["intermediate_relevances"]
-        pixel_relevances_0, pixel_relevances_1 = pairx_results[layer_key]["pixel_relevances"]
+    
+    all_output_images = []
+    for i, (img_np_0, img_np_1) in enumerate(zip(imgs_np_0, imgs_np_1)):
+        output_images = []
+        for layer_key in layer_keys:
+            matches = pairx_results[layer_key]["matches"][i]
+            intermediate_relevance_0, intermediate_relevance_1 = [ir[i] for ir in pairx_results[layer_key]["intermediate_relevances"]]
+            pixel_relevances_0, pixel_relevances_1 = pairx_results[layer_key]["pixel_relevances"]
+            pixel_relevances_0 = [pr[i] for pr in pixel_relevances_0]
+            pixel_relevances_1 = [pr[i] for pr in pixel_relevances_1]
 
-        output_images.append(draw_matches_and_color_maps(img_np_0, img_np_1, matches[:k_lines],
-                                    intermediate_relevance_0, intermediate_relevance_1,
-                                    pixel_relevances_0, pixel_relevances_1, background_opacity=background_opacity))
+            output_images.append(draw_matches_and_color_maps(img_np_0, img_np_1, matches[:k_lines],
+                                        intermediate_relevance_0, intermediate_relevance_1,
+                                        pixel_relevances_0, pixel_relevances_1, background_opacity=background_opacity))
+        
+        all_output_images.append(output_images)
 
     if len(layer_keys) == 1:
-        return output_images[0]
+        return [output_images[0] for output_images in all_output_images]
     
-    return output_images
+    return all_output_images
 
--- demo.py
+++ demo.py
@@ -5,8 +5,15 @@ from PIL import Image
 from xai_dataset import XAIDataset, get_img_pair_from_paths
 from example_loaders import toy_df, wildme_multispecies_miewid
 from core import explain
+import cv2
+from datetime import datetime
+
+
+torch.cuda.empty_cache()
 
 def main():
+    torch.cuda.reset_peak_memory_stats()
+    start_time = datetime.now()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model, img_size, img_transforms = wildme_multispecies_miewid(device)
@@ -14,21 +21,43 @@ def main():
     df = toy_df()
     dataset = XAIDataset(df, img_size, img_transforms)
 
-    img_0, img_1, img_np_0, img_np_1 = dataset.get_img_pair(device, "cow_0_0", "cow_0_1")
-    #img_0, img_1, img_np_0, img_np_1 = get_img_pair_from_paths(device, "data/cow_0_1.jpg", "data/cow_0_0.jpg", img_size, img_transforms)
+    imgs_0 = []
+    imgs_1 = []
+    imgs_np_0 = []
+    imgs_np_1 = []
 
-    pairx_img = explain(img_0,                  # transformed image 0
-                        img_1,                  # transformed image 1
-                        img_np_0,               # untransformed image 0
-                        img_np_1,               # untransformed image 1
+    data_paths = [("cow_0_0.jpg", "cow_0_1.jpg"), ("smores_shoes.jpg", "smores_sink.jpg"), ("bowie_airport.jpg", "bowie_blanket.jpg"), ("bowie_bow.jpg", "bowie_prairie_dog.jpg")]
+    for data_0, data_1 in data_paths:
+        img_0, img_1, img_np_0, img_np_1 = get_img_pair_from_paths(device, f"data/{data_0}", f"data/{data_1}", img_size, img_transforms)
+        imgs_0.append(img_0)
+        imgs_1.append(img_1)
+        imgs_np_0.append(img_np_0)
+        imgs_np_1.append(img_np_1)
+
+
+    #img_0_0, img_1_0, img_np_0_0, img_np_1_0 = dataset.get_img_pair(device, "cow_0_0", "cow_0_1")
+    #img_0_1, img_1_1, img_np_0_1, img_np_1_1 = get_img_pair_from_paths(device, "data/smores_radiator.jpg", "data/smores_sink.jpg", img_size, img_transforms)
+
+    pairx_imgs = explain(torch.cat(imgs_0),                  # transformed image 0
+                        torch.cat(imgs_1),                  # transformed image 1
+                        imgs_np_0,               # untransformed image 0
+                        imgs_np_1,               # untransformed image 1
                         model,                  # model
                         ["backbone.blocks.3"],  # intermediate layer to visualize
                         k_lines=20,             # number of matches to visualize as lines
                         k_colors=10,            # number of matches to visualize as colors
                         )
     
-    pairx_img = Image.fromarray(pairx_img)
-    pairx_img.save("examples/cow_pairx_example.png")
+    #raw_img = cv2.hconcat((img_np_0, img_np_1))
+    #pairx_img = cv2.vconcat((raw_img, pairx_img))
+
+    for i, pairx_img in enumerate(pairx_imgs):
+        pairx_img = Image.fromarray(pairx_img)
+        pairx_img.save(f"examples/cow_pairx_example_{i}.png")
+
+    peak_memory = torch.cuda.max_memory_allocated()  # in bytes
+    print(f"Peak memory allocated: {peak_memory / (1024 ** 3):.2f} GiB")
+    print(f"Time taken: {(datetime.now()-start_time).total_seconds():.2f} seconds")
 
 if __name__ == "__main__":
     main()
